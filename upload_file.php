ef7151fc494a1225f95281011f9c7b2feeb5e44e
Pavlickek <70417844+Pavlickek@users.noreply.github.com>
2021-11-28 22:48:53 -0500

    Update upload_file.php

--- upload_file.php
+++ upload_file.php
@@ -1,6 +1,6 @@
 <?php
 $par = $_FILES['filename1']['tmp_name'];
-$schema = 'C:\OpenServer\domains\crud\XSD\ONIX_BookProduct_Release3.0_reference.xsd';
+$schema = '\XSD\ONIX_BookProduct_Release3.0_reference.xsd';
 $file = simplexml_load_file($par);
 $xml = new DOMDocument();
 $xml->load($par);
